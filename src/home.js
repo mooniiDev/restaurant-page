ac1037236bb10b750522e620a87b698eb28ae5b7
mooniiDev <moonii@riseup.net>
2021-04-14 15:13:25 +0300

    Add homepage module, update header and footer functions

---
+++ src/home.js
@@ -0,0 +1,25 @@
+const home = (() => {
+  function render() {
+    const body = document.querySelector('#content');
+    const aboutDiv = document.createElement('div');
+    const aboutTxt = document.createElement('p');
+    const br1 = document.createElement('br');
+    const br2 = document.createElement('br');
+    aboutDiv.setAttribute('id', 'about');
+    // TEXT NODES
+    const text1 = document.createTextNode('Eat Good Food');
+    const text2 = document.createTextNode('&');
+    const text3 = document.createTextNode('Lift Your Spirit!');
+    // APPENDS
+    aboutTxt.appendChild(text1);
+    aboutTxt.appendChild(br1);
+    aboutTxt.appendChild(text2);
+    aboutTxt.appendChild(br2);
+    aboutTxt.appendChild(text3);
+    aboutDiv.appendChild(aboutTxt);
+    body.appendChild(aboutDiv);
+  }
+  return { render };
+})();
+
+export default home;
--- src/index.js
+++ src/index.js
@@ -1,59 +1,67 @@
-const headerDiv = (() => {
-  // BODY & HEADER
+import home from './home';
+
+const render = (() => {
   const body = document.querySelector('#content');
-  const header = document.createElement('header');
-  header.setAttribute('id', 'header');
-  body.appendChild(header);
 
-  // LOGO
-  const logoDiv = document.createElement('div');
-  const logo = document.createElement('img');
-  logoDiv.setAttribute('id', 'logo');
-  logo.setAttribute('src', 'images/good-mood-food-logo.png');
-  logo.setAttribute('alt', 'Good Mood Food logo');
-  logoDiv.appendChild(logo);
-  header.appendChild(logoDiv);
-
-  // NAVIGATION BAR
-  const navbarDiv = document.createElement('nav');
-  const ul = document.createElement('ul');
-  const navbarLinks = ['HOME', 'MENU', 'CONTACT'];
-  ul.setAttribute('id', 'nav-bar');
-  navbarDiv.appendChild(ul);
-  for (let i = 0; i < 3; i += 1) {
-    const li = document.createElement('li');
-    li.classList.add('nav-link');
-    li.textContent = navbarLinks[i];
-    ul.appendChild(li);
+  function renderHeader() {
+    const header = document.createElement('header');
+    header.setAttribute('id', 'header');
+    // LOGO
+    const logoDiv = document.createElement('div');
+    const logo = document.createElement('img');
+    logoDiv.setAttribute('id', 'logo');
+    logo.setAttribute('src', 'images/good-mood-food-logo.png');
+    logo.setAttribute('alt', 'Good Mood Food logo');
+    logoDiv.appendChild(logo);
+    header.appendChild(logoDiv);
+    // NAVIGATION BAR
+    const navbarDiv = document.createElement('nav');
+    const ul = document.createElement('ul');
+    const navbarLinks = ['HOME', 'MENU', 'CONTACT'];
+    ul.setAttribute('id', 'nav-bar');
+    navbarDiv.appendChild(ul);
+    for (let i = 0; i < 3; i += 1) {
+      const li = document.createElement('li');
+      li.classList.add('nav-link');
+      li.textContent = navbarLinks[i];
+      ul.appendChild(li);
+    }
+    header.appendChild(navbarDiv);
+    body.appendChild(header);
   }
-  header.appendChild(navbarDiv);
 
-  // FOOTER
-  const footerDiv = document.createElement('footer');
-  const footerTxt = document.createElement('p');
-  const text1 = document.createTextNode('☽ Crafted by ');
-  const githubLink = document.createElement('a');
-  const text2 = document.createTextNode('mooniiDev ');
-  const text3 = document.createTextNode('| ');
-  const projectLink = document.createElement('a');
-  const text4 = document.createTextNode('Source ');
-  const text5 = document.createTextNode('☾');
-  footerDiv.setAttribute('id', 'footer');
-  githubLink.classList.add('footer-link');
-  githubLink.target = '_blank';
-  githubLink.setAttribute('href', 'https://github.com/mooniiDev');
-  projectLink.setAttribute('href', 'https://github.com/mooniiDev/restaurant-page');
-  projectLink.target = '_blank';
-  projectLink.classList.add('footer-link');
-  githubLink.appendChild(text2);
-  projectLink.appendChild(text4);
-  footerTxt.appendChild(text1);
-  footerTxt.appendChild(githubLink);
-  footerTxt.appendChild(text3);
-  footerTxt.appendChild(projectLink);
-  footerTxt.appendChild(text5);
-  footerDiv.appendChild(footerTxt);
-  body.appendChild(footerDiv);
+  function renderFooter() {
+    const footer = document.createElement('footer');
+    const footerTxt = document.createElement('p');
+    footer.setAttribute('id', 'footer');
+    // TEXT NODES
+    const text1 = document.createTextNode('☽ Crafted by ');
+    const text2 = document.createTextNode('mooniiDev ');
+    const text3 = document.createTextNode('| ');
+    const text4 = document.createTextNode('Source ');
+    const text5 = document.createTextNode('☾');
+    // LINKS
+    const githubLink = document.createElement('a');
+    const projectLink = document.createElement('a');
+    githubLink.classList.add('footer-link');
+    githubLink.target = '_blank';
+    githubLink.setAttribute('href', 'https://github.com/mooniiDev');
+    projectLink.setAttribute('href', 'https://github.com/mooniiDev/restaurant-page');
+    projectLink.target = '_blank';
+    projectLink.classList.add('footer-link');
+    // TEXTS & LINKS APPENDS
+    githubLink.appendChild(text2);
+    projectLink.appendChild(text4);
+    footerTxt.appendChild(text1);
+    footerTxt.appendChild(githubLink);
+    footerTxt.appendChild(text3);
+    footerTxt.appendChild(projectLink);
+    footerTxt.appendChild(text5);
+    footer.appendChild(footerTxt);
+    body.appendChild(footer);
+  }
 
-  return { body };
+  renderHeader();
+  home.render();
+  renderFooter();
 })();
